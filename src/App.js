10bd2a7c67f981a7275fe5e2c511c669fa3fe8b3
mar1n <30738216+mar1n@users.noreply.github.com>
2021-01-23 19:10:24 +0000

    toggle components

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import { ThemeProvider } from "styled-components";
 import { lightTheme, darkTheme } from "./theme";
 import { GlobalStyles } from "./global";
+import Toggle from "./components/Toggle";
+import Toogle from "./components/Toggle";
 
 function App() {
   const [ theme, setTheme] = useState('light');
@@ -17,8 +19,8 @@ function App() {
     <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
       <>
         <GlobalStyles />
-        <button onClick={toggleTheme}>Toggle theme</button>
-        <h1>It's a light theme!</h1>
+        <Toogle theme={theme} toggleTheme={toggleTheme} />
+        <h1>It's a {theme === 'light' ? 'light theme' : 'dark theme'}!</h1>
         <footer></footer>
       </>
     </ThemeProvider>
