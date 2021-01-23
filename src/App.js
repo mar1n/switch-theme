60a9c6d63bf58a2fa25c99970fc27e947ff328a6
mar1n <30738216+mar1n@users.noreply.github.com>
2021-01-23 20:31:59 +0000

    the useDarkMode hook

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import React, { useState } from "react";
+import React from "react";
 import { ThemeProvider } from "styled-components";
+import { useDarkMode } from "./useDarkMode";
 import { lightTheme, darkTheme } from "./theme";
 import { GlobalStyles } from "./global";
-import Toggle from "./components/Toggle";
 import Toogle from "./components/Toggle";
 
 function App() {
-  const [ theme, setTheme] = useState('light');
-
-  const toggleTheme = () => {
-    if(theme === 'light') {
-      setTheme('dark');
-    } else {
-      setTheme('light');
-    }
-  }
+  const [theme, toggleTheme] = useDarkMode();
+  const themeMode = theme === "light" ? lightTheme : darkTheme;
   return (
-    <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
+    <ThemeProvider theme={themeMode}>
       <>
         <GlobalStyles />
         <Toogle theme={theme} toggleTheme={toggleTheme} />
-        <h1>It's a {theme === 'light' ? 'light theme' : 'dark theme'}!</h1>
+        <h1>It's a {theme === "light" ? "light theme" : "dark theme"}!</h1>
         <footer></footer>
       </>
     </ThemeProvider>
