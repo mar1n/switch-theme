2ec21df9f27dac43f8b6a0d383ad853bd3bd19f3
mar1n <30738216+mar1n@users.noreply.github.com>
2021-01-23 20:55:12 +0000

    using the user's preferred color scheme

--- src/useDarkMode.js
+++ src/useDarkMode.js
@@ -18,13 +18,13 @@ export const useDarkMode = () => {
   };
 
   useEffect(() => {
-    const localTheme = window.localStorage.getItem("theme");
-    if (localTheme) {
-      setTheme(localTheme);
-    } else {
-      setMode("light");
-    }
-    setComponentMounted(true);
+    const localTheme = window.localStorage.getItem('theme');
+    window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches && !localTheme ?
+        setMode('dark') :
+        localTheme ?
+            setTheme(localTheme) :
+            setMode('light');
+        setComponentMounted(true);
   }, []);
 
   return [theme, toggleTheme, componentMounted];
