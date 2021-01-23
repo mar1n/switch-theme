ed8efa3746cc84e5939deeec7716625607a3816d
mar1n <30738216+mar1n@users.noreply.github.com>
2021-01-23 18:22:05 +0000

    add file with stylecomponents styles, add layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { ThemeProvider } from "styled-components";
+import { lightTheme, darkTheme } from "./theme";
+import { GlobalStyles } from "./global";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={lightTheme}>
+      <>
+        <GlobalStyles />
+        <button>Toggle theme</button>
+        <h1>It's a light theme!</h1>
+        <footer></footer>
+      </>
+    </ThemeProvider>
   );
 }
 
