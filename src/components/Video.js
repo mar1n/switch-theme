90250983112966397352504b4bacca793bf46b8c
mar1n <30738216+mar1n@users.noreply.github.com>
2021-01-27 17:14:31 +0000

    responsive  img and video

--- src/components/Video.js
+++ src/components/Video.js
@@ -1,14 +1,15 @@
 import React from "react";
 import Styled from "styled-components";
-import VideoMp4 from "../video/Clouds.mp4";
 
 const Video = () => {
   return (
     <StyledVideo>
       <h1>Video</h1>
       <video width="400" autoPlay muted loop>
-        <source src={VideoMp4} type="video/mp4" />
-        <source src={VideoMp4} type="video/ogg" />
+        <source
+          src="//www-static.cdn-one.com/images/onecom/pages/frontpage/FrontpageHeader-preview.mp4"
+          type="video/mp4"
+        />
         Your browser does not support HTML video.
       </video>
     </StyledVideo>
@@ -19,4 +20,8 @@ export default Video;
 
 const StyledVideo = Styled.div`
     background-color: orangered;
+    video {
+      width: 100%;
+      height: auto;
+    }
 `;
